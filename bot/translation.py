2307b8530b0c121ebcfc3181997a00e5b4fb16de
Unnikuttankk <87984213+Unnikuttankk@users.noreply.github.com>
2021-08-07 14:18:12 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) @AlbertEinsteinTG
+# (c) @uniuni00
 
 class Translation(object):
     
@@ -28,13 +28,5 @@ Now You Are All Set And Ready To Go...
 Just Send Any Text Will Try To Lookup In Channel And Provide You The Link</i>
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code>Adv Auto Filter Bot</code>
-
-<b>➥ Creator</b> : <b><i><a herf="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
-
-<b>➥ Language</b> : <code>Python3</code>
-
-<b>➥ Library</b> : <i><a herf="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0</a></i>
-
-<b>➥ Source Code</b> : <i><a herf="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">GitHub</a></i>
-"""
+   
+ABOUT TEXT =""" </b> OUR CHANNEL </b>t.me/Unlimtedmovie00 
